72ba1a51c7b00053c68f7702e18b3bc118320ff5
Nahid Karim <nahidankur@gmail.com>
2021-02-02 06:49:32 +0600

    update v2

--- passport-setup.js
+++ passport-setup.js
@@ -14,7 +14,7 @@ passport.deserializeUser(function(user, done){
 passport.use(new GoogleStrategy({
   clientID : '392710913316-u8mlrnjv903qkgoa7cv1d49ck6k3t8ej.apps.googleusercontent.com',
   clientSecret : '_XzHlN-Ajy7NHtYm3132IzJr',
-  callbackURL : 'http://localhost:3000/auth/google/callback'
+  callbackURL : 'https://google-oauth-passport.herokuapp.com/google/callback'
 }, (request, accressToken, refreshToken, profile, done)=>{
   console.log(profile)
   return done(null, profile)
